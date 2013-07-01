f04b77ca089ba1f1a59d39dd004b74c9e2a80ec9
karlbennett <where_1ts@yahoo.com>
2013-07-01 23:20:32 +1200

    Updated the User so that it can now handle it's own persistence.

--- src/main/scala/scratch/webapp/spray/data/User.scala
+++ src/main/scala/scratch/webapp/spray/data/User.scala
@@ -1,23 +1,88 @@
 package scratch.webapp.spray.data
 
-import scala.slick.driver.HsqldbDriver._
+import scala.slick.driver.HsqldbDriver.simple._
+import Database.threadLocalSession
 
-/**
- * A simple user case class that contains an email, first, and last names.
- *
- * @author Karl Bennett
- */
-case class User (id: Option[Long], email: String, firstName: String, lastName: String)
+private object UserDataBase {
 
-/**
- * The Slick table mapping for the [[scratch.webapp.spray.data.User]] case class.
- */
+  val dataBase = Database.forURL("jdbc:hsqldb:mem:user", user = "sa", password = "", driver = "org.hsqldb.jdbcDriver")
+}
+
+case class User(id: Option[Long], email: String, firstName: String, lastName: String) {
+
+  import UserDataBase._
+
+  def create: User = {
+
+    dataBase withSession {
+
+      val id = User.forInsert returning User.id insert this
+
+      User(Some(id), this.email, this.firstName, this.lastName)
+    }
+  }
+
+  def update: User = {
+
+    dataBase withSession {
+
+      this.id.foreach(User.queryById(_).update(this))
+    }
+
+    this
+  }
+
+  def delete: User = {
+
+    dataBase withSession {
+
+      this.id.foreach(User.queryById(_).delete)
+    }
+
+    this
+  }
+}
+
 object User extends Table[User]("users") {
   def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
+
   def email = column[String]("email")
+
   def firstName = column[String]("first_name")
+
   def lastName = column[String]("last_name")
-  def * = id.? ~ email ~ firstName ~ lastName <> (User.apply _, User.unapply _)
-  def forInsert = email ~ firstName ~ lastName <> ({ t => User(None, t._1, t._2, t._3)}, { (u: User) => Some((u.email, u.firstName, u.lastName))})
+
+  def * = id.? ~ email ~ firstName ~ lastName <>(User.apply _, User.unapply _)
+
+  def forInsert = email ~ firstName ~ lastName <>( {
+    t => User(None, t._1, t._2, t._3)
+  }, {
+    (u: User) => Some((u.email, u.firstName, u.lastName))
+  })
+
+  def queryById(id: Long)(implicit session: Session): Query[User.type, User] = {
+
+    for {user <- User if user.id === id} yield user
+  }
+
+  def findById(userId: Long): Option[User] = {
+
+    UserDataBase.dataBase withSession {
+
+      val query = for {
+        u <- User if u.id === userId
+      } yield u
+
+      query.firstOption
+    }
+  }
+
+  def findAll: List[User] = {
+
+    UserDataBase.dataBase withSession {
+
+      Query(User).list
+    }
+  }
 }
 
